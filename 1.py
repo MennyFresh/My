6e042fedbfe9192c44a214e3880bf2fc47fe6333
MennyFresh <stas997@ukr.net>
2023-11-27 20:20:28 +0000

    First commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+def printCategory(age):
+   age = 70
+ if age > 18:
+        print('Adult')
+elif age > 65:
+        print('Senior Citizen')
+else:
+        print('Child')
---
+++ for-loop.py
@@ -0,0 +1,5 @@
+print("cout to 10!")
+for x in range (0,11):
+    print(x)
+    
+    print(2)
